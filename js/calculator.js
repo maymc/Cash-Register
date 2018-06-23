ebe9546d9eefd149d6291d12485d8db661ead794
May Chen <maychen@Mays-MacBook-Pro.local>
2018-06-22 19:14:16 -1000

    Created all buttons and the screen

---
+++ js/calculator.js
@@ -0,0 +1,16 @@
+(function(){
+
+    console.log("Hello World!")
+
+function testFunction(){
+    console.log("Yay my button works!");
+}
+    var demoButton = document.getElementById("testButton");
+    demoButton.addEventListener("click", testFunction);
+
+
+
+
+
+    
+}());
